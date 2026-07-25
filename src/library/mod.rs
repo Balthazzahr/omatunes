48b95e00e9e8aba7fcc3c81bbecef7f0c73fb0a9
Balthazzahr <Balthazzahr@users.noreply.github.com>
2026-07-25 17:11:43 +1000

    Auto-commit: local modifications sync

--- src/library/mod.rs
+++ src/library/mod.rs
@@ -3,4 +3,5 @@ pub mod scanner;
 pub mod smart_playlist;
 
 pub use models::Track;
-pub use scanner::{load_cover, scan_folder, write_tags};
+pub use scanner::{load_cache, load_cover, save_cache, scan_folder, write_tags};
+
